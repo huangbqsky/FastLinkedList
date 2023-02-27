221e509f0375e030916940f11b9ffd5378a017ca
huangbqsky <huangbqsky@gmail.com>
2023-02-27 18:34:37 +0800

    update tests

--- tests/test.rs
+++ tests/test.rs
@@ -3,141 +3,141 @@ use linked_list::FastLinkedList;
 
 #[test]
 fn test_push_remove() {
-    let mut lur = FastLinkedList::new();
-    let one = lur.push("1".to_string());
-    let tow = lur.push("2".to_string());
-    let three = lur.push("3".to_string());
+    let mut linked = FastLinkedList::new();
+    let one = linked.push("1".to_string());
+    let tow = linked.push("2".to_string());
+    let three = linked.push("3".to_string());
 
-    assert_eq!(lur.remove(one), Some("1".to_string()));
-    assert_eq!(lur.remove(tow), Some("2".to_string()));
-    assert_eq!(lur.remove(three), Some("3".to_string()));
+    assert_eq!(linked.remove(one), Some("1".to_string()));
+    assert_eq!(linked.remove(tow), Some("2".to_string()));
+    assert_eq!(linked.remove(three), Some("3".to_string()));
 
-    let one = lur.push("1".to_string());
-    let tow = lur.push("2".to_string());
-    let three = lur.push("3".to_string());
+    let one = linked.push("1".to_string());
+    let tow = linked.push("2".to_string());
+    let three = linked.push("3".to_string());
 
-    assert_eq!(lur.remove(three), Some("3".to_string()));
-    assert_eq!(lur.remove(tow), Some("2".to_string()));
-    assert_eq!(lur.remove(one), Some("1".to_string()));
+    assert_eq!(linked.remove(three), Some("3".to_string()));
+    assert_eq!(linked.remove(tow), Some("2".to_string()));
+    assert_eq!(linked.remove(one), Some("1".to_string()));
 
-    let one = lur.push("1".to_string());
-    let tow = lur.push("2".to_string());
-    let three = lur.push("3".to_string());
+    let one = linked.push("1".to_string());
+    let tow = linked.push("2".to_string());
+    let three = linked.push("3".to_string());
 
-    assert_eq!(lur.remove(tow), Some("2".to_string()));
-    assert_eq!(lur.remove(three), Some("3".to_string()));
-    assert_eq!(lur.remove(one), Some("1".to_string()));
+    assert_eq!(linked.remove(tow), Some("2".to_string()));
+    assert_eq!(linked.remove(three), Some("3".to_string()));
+    assert_eq!(linked.remove(one), Some("1".to_string()));
 
-    let one = lur.push("1".to_string());
-    let tow = lur.push("2".to_string());
-    let three = lur.push("3".to_string());
+    let one = linked.push("1".to_string());
+    let tow = linked.push("2".to_string());
+    let three = linked.push("3".to_string());
 
-    assert_eq!(lur.remove(one), Some("1".to_string()));
-    assert_eq!(lur.remove(three), Some("3".to_string()));
-    assert_eq!(lur.remove(tow), Some("2".to_string()));
+    assert_eq!(linked.remove(one), Some("1".to_string()));
+    assert_eq!(linked.remove(three), Some("3".to_string()));
+    assert_eq!(linked.remove(tow), Some("2".to_string()));
 }
 
 #[test]
 fn test_move_front() {
-    let mut lur: FastLinkedList<usize> = FastLinkedList::new();
+    let mut linked: FastLinkedList<usize> = FastLinkedList::new();
 
-    let one = lur.push(1);
-    lur.move_front(one).unwrap();
-    assert_eq!(lur.remove(one), Some(1));
+    let one = linked.push(1);
+    linked.move_front(one).unwrap();
+    assert_eq!(linked.remove(one), Some(1));
 
-    let one = lur.push(1);
-    let tow = lur.push(2);
-    let three = lur.push(3);
+    let one = linked.push(1);
+    let tow = linked.push(2);
+    let three = linked.push(3);
 
-    lur.move_front(one).unwrap();
-    assert_eq!(lur.remove(one), Some(1));
-    lur.move_front(tow).unwrap();
-    assert_eq!(lur.remove(tow), Some(2));
-    lur.move_front(three).unwrap();
-    assert_eq!(lur.remove(three), Some(3));
+    linked.move_front(one).unwrap();
+    assert_eq!(linked.remove(one), Some(1));
+    linked.move_front(tow).unwrap();
+    assert_eq!(linked.remove(tow), Some(2));
+    linked.move_front(three).unwrap();
+    assert_eq!(linked.remove(three), Some(3));
 
     for i in 1..10000 {
-        lur.push(i);
+        linked.push(i);
     }
 
-    let one = lur.push(1);
-    let tow = lur.push(2);
-    let three = lur.push(3);
+    let one = linked.push(1);
+    let tow = linked.push(2);
+    let three = linked.push(3);
 
-    assert_eq!(lur.get(one), Some(&1));
-    assert_eq!(lur.get_mut(tow), Some(&mut 2));
+    assert_eq!(linked.get(one), Some(&1));
+    assert_eq!(linked.get_mut(tow), Some(&mut 2));
 
-    lur.move_front(three).unwrap();
-    assert_eq!(lur.remove(three), Some(3));
-    lur.shrink_to_fit();
-    lur.move_front(tow).unwrap();
-    assert_eq!(lur.remove(tow), Some(2));
-    lur.move_front(one).unwrap();
-    assert_eq!(lur.remove(one), Some(1));
+    linked.move_front(three).unwrap();
+    assert_eq!(linked.remove(three), Some(3));
+    linked.shrink_to_fit();
+    linked.move_front(tow).unwrap();
+    assert_eq!(linked.remove(tow), Some(2));
+    linked.move_front(one).unwrap();
+    assert_eq!(linked.remove(one), Some(1));
 
-    let one = lur.push(1);
-    let tow = lur.push(2);
-    let three = lur.push(3);
+    let one = linked.push(1);
+    let tow = linked.push(2);
+    let three = linked.push(3);
 
-    lur.move_front(one).unwrap();
-    assert_eq!(lur.remove(tow), Some(2));
-    lur.move_front(three).unwrap();
-    assert_eq!(lur.remove(one), Some(1));
-    lur.move_front(three).unwrap();
-    assert_eq!(lur.remove(three), Some(3));
+    linked.move_front(one).unwrap();
+    assert_eq!(linked.remove(tow), Some(2));
+    linked.move_front(three).unwrap();
+    assert_eq!(linked.remove(one), Some(1));
+    linked.move_front(three).unwrap();
+    assert_eq!(linked.remove(three), Some(3));
 }
 
 #[test]
-fn lur_linked_remove_last() {
-    let mut lur: FastLinkedList<usize> = FastLinkedList::new();
-    assert_eq!(lur.remove_last(), None);
-    lur.push(1);
-    assert_eq!(lur.len(), 1);
-    assert_eq!(lur.remove_last(), Some(1));
+fn linked_linked_remove_last() {
+    let mut linked: FastLinkedList<usize> = FastLinkedList::new();
+    assert_eq!(linked.remove_last(), None);
+    linked.push(1);
+    assert_eq!(linked.len(), 1);
+    assert_eq!(linked.remove_last(), Some(1));
 
-    lur.push(1);
-    lur.push(2);
-    lur.push(3);
+    linked.push(1);
+    linked.push(2);
+    linked.push(3);
 
-    assert_eq!(lur.remove_last(), Some(1));
-    assert_eq!(lur.remove_last(), Some(2));
-    assert_eq!(lur.remove_last(), Some(3));
-    assert_eq!(lur.remove_last(), None);
+    assert_eq!(linked.remove_last(), Some(1));
+    assert_eq!(linked.remove_last(), Some(2));
+    assert_eq!(linked.remove_last(), Some(3));
+    assert_eq!(linked.remove_last(), None);
 
-    let one = lur.push(1);
-    lur.push(2);
-    let three = lur.push(3);
-    lur.move_front(one).unwrap();
-    assert_eq!(lur.remove_last(), Some(2));
-    lur.move_front(three).unwrap();
-    assert_eq!(lur.remove_last(), Some(1));
-    assert_eq!(lur.remove_last(), Some(3));
+    let one = linked.push(1);
+    linked.push(2);
+    let three = linked.push(3);
+    linked.move_front(one).unwrap();
+    assert_eq!(linked.remove_last(), Some(2));
+    linked.move_front(three).unwrap();
+    assert_eq!(linked.remove_last(), Some(1));
+    assert_eq!(linked.remove_last(), Some(3));
 
-    lur.push(1);
-    lur.push(2);
-    lur.push(3);
-    lur.clear();
-    assert_eq!(lur.remove_last(), None);
-    lur.shrink_to_fit();
+    linked.push(1);
+    linked.push(2);
+    linked.push(3);
+    linked.clear();
+    assert_eq!(linked.remove_last(), None);
+    linked.shrink_to_fit();
 }
 
 #[test]
 fn test_iter() {
-    let mut lur = FastLinkedList::new();
+    let mut linked = FastLinkedList::new();
     for i in 1..10i32 {
-        lur.push(i);
+        linked.push(i);
     }
-    println!("{}", lur.len());
+    println!("{}", linked.len());
 
-    for (key, value) in lur.iter() {
+    for (key, value) in linked.iter() {
         println!("key:{key},value:{value}")
     }
 
-    for (key, value) in lur.iter().rev() {
+    for (key, value) in linked.iter().rev() {
         println!("rev key:{key},value:{value}")
     }
 
-    let mut iter = lur.iter();
+    let mut iter = linked.iter();
     for i in 0..9 {
         if i % 2 == 0 {
             if let Some((key, value)) = iter.next() {
@@ -154,8 +154,8 @@ fn test_iter() {
         }
     }
 
-    lur.remove_last();
-    let mut iter = lur.iter();
+    linked.remove_last();
+    let mut iter = linked.iter();
     assert_eq!(iter.len(), 8);
     iter.next();
     assert_eq!(iter.len(), 7);
@@ -165,29 +165,29 @@ fn test_iter() {
 
 #[test]
 fn test_iter_mut() {
-    let mut lur = FastLinkedList::new();
+    let mut linked = FastLinkedList::new();
     for i in 1..10i32 {
-        lur.push(i);
+        linked.push(i);
     }
 
-    for (_, item) in lur.iter_mut() {
+    for (_, item) in linked.iter_mut() {
         *item += 1;
     }
 
-    for (key, value) in lur.iter() {
+    for (key, value) in linked.iter() {
         println!("key:{key},value:{value}")
     }
 
     for key in 0..9usize {
         let p = key as i32 + 2;
-        assert_eq!(lur.get(key), Some(&p))
+        assert_eq!(linked.get(key), Some(&p))
     }
 
-    for (key, value) in lur.iter_mut().rev() {
+    for (key, value) in linked.iter_mut().rev() {
         println!("rev key:{key},value:{value}")
     }
 
-    let mut iter = lur.iter_mut();
+    let mut iter = linked.iter_mut();
     for i in 0..9 {
         if i % 2 == 0 {
             if let Some((key, value)) = iter.next() {
@@ -204,40 +204,40 @@ fn test_iter_mut() {
         }
     }
 
-    lur.remove_last();
-    let mut iter = lur.iter_mut();
+    linked.remove_last();
+    let mut iter = linked.iter_mut();
     assert_eq!(iter.len(), 8);
     iter.next();
     assert_eq!(iter.len(), 7);
     iter.next_back();
     assert_eq!(iter.len(), 6);
 
-    lur.clear();
+    linked.clear();
 }
 
 #[test]
 fn test_into_iter() {
-    let mut lur = FastLinkedList::new();
+    let mut linked = FastLinkedList::new();
     for i in 1..10i32 {
-        lur.push(i);
+        linked.push(i);
     }
 
-    for value in lur.into_iter() {
+    for value in linked.into_iter() {
         println!("value:{value}")
     }
 
-    let mut lur = FastLinkedList::new();
+    let mut linked = FastLinkedList::new();
     for i in 1..10i32 {
-        lur.push(i);
+        linked.push(i);
     }
 
-    let ref_lur = &lur;
-    for (key, value) in ref_lur.into_iter() {
+    let ref_linked = &linked;
+    for (key, value) in ref_linked.into_iter() {
         println!("key:{key},value:{value}")
     }
 
-    let ref_mut_lur = &mut lur;
-    for (key, value) in ref_mut_lur.into_iter() {
+    let ref_mut_linked = &mut linked;
+    for (key, value) in ref_mut_linked.into_iter() {
         println!("mut key:{key},value:{value}")
     }
 }
